b9a10547b1adc876463cdb4d62cdecbc3039537e
TeoL <teo.ladouche@hotmail.com>
2026-01-18 14:43:26 +0100

    GREEN - Implement laboratory valid intialization

--- src/Laboratory.js
+++ src/Laboratory.js
@@ -1,10 +1,13 @@
 class Laboratory {
     constructor(substances) {
-
+        this.stock = {};
+        substances.forEach(substance => {
+            this.stock[substance] = 0;
+        });
     }
 
     getQuantity(substance) {
-
+        return this.stock[substance];
     }
 }
 
