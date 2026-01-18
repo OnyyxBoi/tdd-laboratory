20fd70cd6dabc62b29f3f1596de146c6bce0aa72
TeoL <teo.ladouche@hotmail.com>
2026-01-18 14:46:57 +0100

    GREEN - Implement laboratory initialization errors handling

--- src/Laboratory.js
+++ src/Laboratory.js
@@ -1,5 +1,13 @@
 class Laboratory {
     constructor(substances) {
+        if (!Array.isArray(substances)) {
+            throw new Error("Substances must be an array");
+        }
+
+        if (!substances.every(s => typeof s === 'string')) {
+            throw new Error("All substances must be strings");
+        }
+
         this.stock = {};
         substances.forEach(substance => {
             this.stock[substance] = 0;
